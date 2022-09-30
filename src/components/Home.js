faf2328e090d015f39a23882e44dc488deb94375
PRAMOD <pramodnakandara@gmail.com>
2022-10-01 02:06:50 +0530

    new12

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,89 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 import { useUserAuth } from '../context/UserAuthContext'
-
+import axios from "axios";
 function Home() {
     const { user, logOut } = useUserAuth();
+    console.log(user.displayName)
+    console.log(user.email)
+    console.log()
+    const [email, setEmail] = useState("");
+    const [payload, setPayload] = useState("");
+    const [password, setPassword] = useState("");
+    const [getname, setGetname] = useState([]);
+    // const [getemail, setGetemail] = useState("");
+    // const [getpayload, setGetpayload] = useState("");
+    // const newLOguser1 = {
+    //     email,
+    //     password,
+    //     payload
+    // }
+
+    // axios.post("http://localhost:8090/loguser/add", newLOguser1).then(() => {
+    //     alert("user add")
+    //     console.log("addd")
+
+    // }).catch((err) => {
+    //     alert(err)
+    // })
+
+
+    useEffect(() => {
+        getlogdata()
+        googlelogin()
+    }, [email]);
+  
+
+    // const getlogdata = async () => {
+    //     try {
+    //         axios.get("http://ec2-54-91-203-24.compute-1.amazonaws.com:8090/loguser/").then((res) => {
+
+    //             console.log(res)
+
+    //             // setGetemail(res.data.email)
+    //             // setGetpayload(res.data.payload)
+
+    //         }).catch((err) => {
+    //             alert(err)
+    //         })
+    //     } catch (error) {
+    //         console.log(error.message)
+    //     }
+    // }
+
+    const getlogdata = async () => {
+        axios.get("http://ec2-54-91-203-24.compute-1.amazonaws.com:8090/loguser/").then((res) => {
+
+            console.log(res.data[0])
+            setGetname(res.data)
+            // setGetemail(res.data.email)
+            // setGetpayload(res.data.payload)
+
+        }).catch((err) => {
+            alert(err)
+        })
+    }
+    const googlelogin = async () => {
+
+        setEmail(user.email)
+        setPassword(user.photoURL)
+        setPayload(user.displayName)
+        try {
+            const newLOguser1 = {
+                email,
+                password,
+                payload
+            }
+            axios.post("http://ec2-54-91-203-24.compute-1.amazonaws.com:8090/loguser/add", newLOguser1).then(() => {
+
+                console.log("addd")
+
+            }).catch((err) => {
+                alert(err)
+            })
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
 
     const loguthandler = async () => {
         try {
@@ -15,6 +96,15 @@ function Home() {
         <div>
             {user && user.email}
             <button onClick={loguthandler}>Logout</button>
+            <div> {getname.map((val, key) => {
+                return <div key={key} className="food">
+                    <h1>{val.name}</h1><h1>{val.payload}</h1>
+
+
+                </div>
+            })}</div>
+            <div><button onClick={googlelogin}>lklk</button></div>
+            
         </div>
     )
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -4,27 +4,52 @@ import { Link, useNavigate } from "react-router-dom";
 import GoogleButton from "react-google-button";
 import { useUserAuth } from "../context/UserAuthContext"
 import { Alert } from "react-bootstrap";
+import axios from "axios";
+//import GoogleLogin from "./GoogleLogin";
 
 
-function Login() {
+const Login = ({ children }) => {
 
     const [email, setEmail] = useState("");
     const [error, setError] = useState("");
+    const [payload, setPayload] = useState("vvdvdvdvdvdvv");
     const [password, setPassword] = useState("");
-    const { logIn, googleSignIn } = useUserAuth();
+    const { logIn, googleSignIn, user } = useUserAuth();
+    // let { user } = useUserAuth();
+
     let navigate = useNavigate();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         setError("")
+        setPayload("dccdccd")
         try {
             await logIn(email, password);
+
             navigate("/home");
+            console.log(email)
+            console.log(password)
+            console.log(payload)
+            const newLOguser1 = {
+                email,
+                password,
+                payload
+            }
+            axios.post("http://ec2-54-91-203-24.compute-1.amazonaws.com:8090/loguser/add", newLOguser1).then(() => {
+                alert("user add")
+                console.log("addd")
+
+            }).catch((err) => {
+                alert(err)
+            })
         } catch (err) {
             setError(err.message);
         }
+
     }
+
     const handleGooglesignIn = async (e) => {
+
         e.preventDefault();
         try {
             await googleSignIn();
@@ -33,6 +58,9 @@ function Login() {
             setError(error.message);
         }
     }
+
+
+    console.log(user)
     return (
         <div>
             <div className="wrapper">
@@ -57,6 +85,7 @@ function Login() {
                     </div>
                     <button className="btn mt-3">Login</button>
                     <GoogleButton className="btn mt-3" onClick={handleGooglesignIn}>Google Login</GoogleButton>
+
                 </form>
                 <div className="text-center fs-6">
                     or <Link to="/signup" >Sign up</Link>
